d3568df96508a30ca15e1c5883a711a37f96f5a6
Nico Albanese <nico.albanese@vercel.com>
2025-03-03 10:17:19 +0000

    init

---
+++ api/events.ts
@@ -0,0 +1,12 @@
+import { waitUntil } from "@vercel/functions";
+
+export const GET = async () => {
+  waitUntil(
+    (async () => {
+      console.log("waiting...");
+      await new Promise((resolve) => setTimeout(resolve, 11000));
+      console.log("done");
+    })()
+  );
+  return new Response("Hello World!");
+};
